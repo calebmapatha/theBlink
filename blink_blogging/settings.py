7685dbb714900a483c3db692e1555620858b067f
calebmapatha <calebmapatha@gmail.com>
2020-08-18 04:50:09 +0200

    gey

--- blink_blogging/settings.py
+++ blink_blogging/settings.py
@@ -15,7 +15,7 @@ import os
 from typing import List, Any
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DEVELOPMENT_MODE_ENABLED = True
+DEVELOPMENT_MODE_ENABLED = False
 if DEVELOPMENT_MODE_ENABLED:
     try:
         from blink_blogging.settings_conf.development import *
